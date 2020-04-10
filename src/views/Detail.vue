63afdd345a0d29e7036dfae6260a16d5b4a928e1
Phycho <phycho_ljx@163.com>
2020-04-11 02:12:59 +0800

    详情页面

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,19 +1,53 @@
 <template>
-  <div class="datail">
+  <div class="datail" v-if="filmInfo">
     <div class="top">
       <div class="left">
         <img :src="filmInfo.image" alt="">
       </div>
+      <div class="right">
+        <h3>{{filmInfo.titleCn}}</h3>
+        <h5>{{filmInfo.titleEn}}</h5>
+        <span>{{filmInfo.runTime}}</span>
+        <p>
+          <span v-for="(type,index) in filmInfo.type" :key="index">{{type}}</span>
+        </p>
+        <p>
+          <span v-for="(time,index) in filmInfo.release" :key="index">{{time}}/</span>
+        </p>
+        <nav>
+          <span>我想看</span>
+          <span>我要评分</span>
+        </nav>
+        <i>{{filmInfo.rating}}</i>
+      </div>
+      <p class="text">{{filmInfo.commonSpecial}}</p>
     </div>
+    <div class="center">
+      <p :class="{details:isActive}">{{filmInfo.content}}</p>
+      <i class="iconfont " @click="isActive = !isActive" :class="isActive?'icon-jiantou-copy-copy':'icon-jiantou'"></i>
+    </div>
+    <div class="actors">
+      <p @click="isPhotoShow=true">演职人员</p>
+      <ul>
+        <li>演职人员</li>
+      </ul>
+    </div>
+    <Actor v-show="isPhotoShow"></Actor>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Actor from './detail/Actor'
 export default {
+  components: {
+    Actor
+  },
   data () {
     return {
-      filmInfo: []
+      filmInfo: null,
+      isActive: true,
+      isPhotoShow: false
     }
   },
   created () {
@@ -25,5 +59,90 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+  .top{
+    display: flex;
+    flex-wrap: wrap;
+    padding-left: 18px;
+    font-family: "helvetica neue","微软雅黑";
+    .left{
+      overflow: hidden;
+      background: #fff;
+      margin-right: 6px;
+      padding: 2px;
+      width: 123px;
+      height: 182px;
+      // float: left;
+      img{
+        width: 100%;
+        height: auto;
+      }
+    }
+    .right{
+      // float: right;
+      h3{
+        margin-top:5px;
+        font-size: 18px;
+      }
+      h5{
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        font-size: 16px;
+        margin-bottom: 15px;
+      }
+      p{
+        margin: 2px ;
+      }
+      nav{
+        display: flex;
+        span{
+          display: block;
+          width: 88px;
+          height: 36px;
+          line-height: 36px;
+          margin: 10px 0 0 5px;
+          text-align: center;
+          border: 1px solid #999;
+          border-radius: 18px;
+          font-size: 15px;
+        }
+      }
+      i{
+        position: absolute;
+        top: 50px;
+        right: 10px;
+        width: 30px;
+        height: 30px;
+        background: #659d0e;
+        color: #fff;
+        font-size: 16px;
+        text-align: center;
+        line-height: 30px;
+      }
+    }
+    .text{
+      color: #fd8900;
+      font-size: 18px;
+      margin-left:78px ;
+    }
+  }
+  .center{
+    margin: 5px 0;
+    padding: 10px 15px;
+    border-top: 10px solid #f6f6f6;
+    border-bottom: 10px solid #f6f6f6;
+    text-align: center;
+    p{
+      font-size: 18px;
+
+      margin-bottom: 5px;
+      // text-overflow: ellipsis;
+      // white-space: nowrap;
+    }
+    .details{
+      height: 50px;
+      overflow: hidden;
+    }
+  }
 </style>
---
+++ src/views/detail/Actor.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="photo">
+      <van-nav-bar
+        title="演职员表"
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"
+        @click-right="onClickRight"
+      />
+      <van-index-bar :index-list="indexList" >
+      <div v-for="(data,index) in actorList" :key="index" >
+        <van-index-anchor index="index">{{data.typeName}}</van-index-anchor>
+        <div v-for="item in data.persons" :key="item.id" class="photoList">
+          <img :src="item.image" alt="" >
+          <div class="right">
+            <van-cell  :title="item.name" />
+            <span>{{item.nameEn}}</span>
+          </div>
+        </div>
+      </div>
+    </van-index-bar>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+import Vue from 'vue'
+import { IndexBar, IndexAnchor, Cell, NavBar, Toast } from 'vant'
+Vue.use(Cell)
+Vue.use(IndexBar)
+Vue.use(IndexAnchor)
+Vue.use(NavBar)
+Vue.use(Toast)
+export default {
+  data () {
+    return {
+      indexList: [],
+      actorList: []
+    }
+  },
+  created () {
+    axios.get('/Service/callback.mi/Movie/MovieCreditsWithTypes.api?movieId=264258&t=20204111185728169').then(res => {
+      this.actorList = res.data.types
+      console.log(this.actorList)
+    })
+  },
+  methods: {
+    onClickLeft () {
+      Toast('返回')
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+  .photo{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #ffffff;
+    .photoList{
+      // display: flex;
+      img{
+        width: 70px;
+        height: 70px;
+
+      }
+    }
+  }
+</style>
